350e7a12fec3194391bc69e9e7ab4be00e943603
22p23i0006-วรัฏฐา <30413309+weaq@users.noreply.github.com>
2020-09-22 23:03:52 +0700

    Initial Commit

---
+++ image-url-downloader.py
@@ -0,0 +1,36 @@
+
+import requests
+
+# Read txt url from file
+file = open('url.txt', 'r')
+# Read line to list
+lines = file.readlines()
+
+for i in range(len(lines)) :
+
+    line = lines[i].rstrip()
+    print('Line ' + str(i) + ' : ' + line)
+
+    # Remove any leading and trailing whitespaces include
+    url = line.strip()
+
+    # Get response from url
+    response = requests.get(url)
+
+    # Check url status and content type
+    if response.status_code == 200 and response.headers['Content-Type'] == 'image/jpeg':
+        # Get file name from url
+        first = url.rfind("/")
+        last = url.rfind(".")
+        fileName = url[first+1:last]
+        # Create jpg file
+        file = open('img/' + fileName + '.jpg', 'wb')
+        # Write content
+        file.write(response.content)
+        # Close file
+        file.close()
+        msg = '\t Download .jpg succesfuly.'
+    else :
+        msg = '\t Content-Type:' + response.headers['Content-Type']
+
+    print(msg)
